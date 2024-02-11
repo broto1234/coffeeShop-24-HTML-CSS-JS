40055e36fab229b294889072ddf09e77053ed568
Satyobroto <satyo97du@gmail.com>
2024-02-11 16:09:37 +0100

    commit message

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-//window.onload = function () {
-
-    //var el = document.createElement("div");
-    //el.innerHTML = "RABBIT TEST ";
-    //var div = document.getElementById("headline");
-    //insertAfter(div, el);
-
-    /* FUNCTIONS */
-
-    // Inserts newNode after referenceNode
-    //function insertAfter(referenceNode, newNode) {
-      //  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-    //}
-//};
-
 //------Array Object for search list------- 
 const searchItemsList = [
   {
